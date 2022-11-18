c955e893e9562a89b8a7d3c87770f71ac889fc7c
dharmeshphd <93026310+dharmeshphd@users.noreply.github.com>
2022-11-18 18:51:31 +0530

    Update database.connection.js

--- src/config/database.connection.js
+++ src/config/database.connection.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
+// This is not safe place to store user name and password of database, but i keep it here because ease to explain
+user = "admin "
+pass = "cAmetbQDODTBN0J4"
+
+const mongo_db_URI = `mongodb+srv://admin:${pass}@learningdb.at8s5s9.mongodb.net/my-shop?retryWrites=true&w=majority`
+
 const databaseConnection = async () => {
   try {
-    const mongoURI = `mongodb://localhost:27017/software_co`;
-    await mongoose.connect(mongoURI, {
+    // const mongoURI = `mongodb://localhost:27017/software_co`;
+    await mongoose.connect(mongo_db_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
