71e271582a1e46e335137306dd091794b9dd0419
dharmeshphd <93026310+dharmeshphd@users.noreply.github.com>
2022-11-19 16:06:07 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect ,Redirect} from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { PropTypes } from "prop-types";
 import { getProducts } from "../redux/actions/product";
 import Carousel from "../components/Carousel";
 import { addProductToCart } from "../redux/actions/cart";
-
 
 const Home = ({ productList, getProducts, auth, cart, addProductToCart }) => {
   let cart_products = cart.products;
@@ -30,8 +29,12 @@ const Home = ({ productList, getProducts, auth, cart, addProductToCart }) => {
 
   return (
     <div className="container">
-      {/* <h1 className="w-100 text-center mt-2 mb-2">Shop Now</h1> */}
+
       <Carousel />
+      <h4 className="mt-2 mb-2">For development</h4>
+      <p>- To add product in database, login as seller.</p>
+      <p>- To give a permissions to user to sell = login as admin, go to /users and select user and gave permission</p>
+      <hr/>
       <div className="row">
         {productList.map((p, index) => (
           <div className="col-lg-3 col-md-4 card mb-4" key={index}>
@@ -47,16 +50,19 @@ const Home = ({ productList, getProducts, auth, cart, addProductToCart }) => {
                 {p.price}
               </p>
               {/* <p className="card-text mt-0 mb-0">{p.description}</p> */}
-              <button className="btn btn-outline-success mb-2 w-100 btn-sm"
-              onClick={(e)=>{
-                navigate('/product',{state:p});
-              }}
-              >Buy Now</button>
+              <button
+                className="btn btn-outline-success mb-2 w-100 btn-sm"
+                onClick={(e) => {
+                  navigate("/product", { state: p });
+                }}
+              >
+                Buy Now
+              </button>
               <br></br>
               <button
                 className="btn btn-outline-info w-100 btn-sm"
                 onClick={(e) => {
-                  cart_products.push(p)
+                  cart_products.push(p);
                   addProductToCart(cart_products);
                 }}
               >
